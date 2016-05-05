0d26ab6fda87f31d184831c42ab6035ee5871912
Jonathan Minton <nate.minton@gmail.com>
2016-05-05 17:41:53 +0100

    save updated seg explorer script - including using pacman for package management and starting to automate production of metrics using purrr

--- scripts/explore_segregation_measures_in_r.R
+++ scripts/explore_segregation_measures_in_r.R
@@ -1,30 +1,19 @@
 # Scoping and/or implementation of measures of segregation
 
-
 rm(list = ls())
 
-require(readxl)
-require(xlsx)
-require(readr)
+# trying out pacman package
 
-require(spdep)
-require(maptools)
-require(rgdal)
+install.packages("pacman")
 
-require(car)
-require(stringr)
-require(plyr)
-require(tidyr)
-require(dplyr)
-
-require(seg)
-require(OasisR)
-# This looks a more clearly laid out and comprehensive alternative 
-# to seg
+pacman::p_load(
+  readxl, xlsx, readr, spdep, 
+  maptools, rgdal, car, stringr, 
+  purrr, tidyr, dplyr, seg, OasisR,
+  ggplot2
+)
 
 #https://cran.r-project.org/web/packages/OasisR/OasisR.pdf
-
-require(ggplot2)
 
 # Load some kind of shapefile containing the attributes 
 
@@ -33,17 +22,23 @@ dta <- readOGR(
   layer = "accom_2001"                     
 )    
 
-# nhd_matrix <- nb2mat(
-#   poly2nb(
-#     dta, queen = FALSE), 
-#   style = "B",
-#   zero.policy = T
-# )
+nhd_matrix <- nb2mat(
+  poly2nb(
+    dta, queen = FALSE),
+  style = "B",
+  zero.policy = T
+) # NOTE: Takes a while to calculate!
+
 
 
 # unevenness 
 
 dissim_oasis <- DI(dta@data[,c("house", "nonhouse")])
+
+d_adj_oasis <- Morill(dta@data[,c("house", "nonhouse")], nhd_matrix) 
+
+adj <- dissim_oasis - d_adj_oasis
+
 
 # dissim_scores <- dissim(
 #   x = dta, 
@@ -55,20 +50,8 @@ dissim_oasis <- DI(dta@data[,c("house", "nonhouse")])
 # Fails for spatially adjusted measures 
 # because cannot handle empty neighbours 
 
-dissim_scores <- dissim(
-  x = dta, 
-  data = dta@data[,c("house", "nonhouse")],
-  adjust = F
-)
-
-# Values match
 
 # isolation
-
-iso_scores <- seg::spseg(
-  x = dta,
-  data = dta@data[, c("house", "nonhouse")]
-  )
 
 iso_scores2 <- OasisR::xPx(dta@data[,c("house", "nonhouse")])
 
@@ -77,8 +60,6 @@ iso_scores2 <- OasisR::xPx(dta@data[,c("house", "nonhouse")])
 # Adjusted isolation (Eta2)
 
 eta2 <- OasisR::Eta2(dta@data[,c("house", "nonhouse")])
-
-
 
 # centralisation
 
@@ -97,35 +78,136 @@ rce_glasgow <- RCE(
   center = 573
 )
 
-# concentration 
-# trying out for glasgow at the moment
 
-areas <- area(dta)
+# Automating/standardising the above 
 
-rco_glasgow <- RCO(
-  x = dta@data[,c("house", "nonhouse")],
-  a = areas
-) 
-
-# produced a value of -2.22,
-# when the limits are reported as -1 to 1
-
-# The other value is 0.713
-# Which is close to the value calculated 
-# in geoseg analyser (0.729)
-
-# This could be an issue with the RCO index itself:
-#The following paper seems to suggest that the index can become 
-# unbounded for some types of data:
-# http://sf.oxfordjournals.org/content/76/3/1115.long
-
-# The results (0.71 and -2.21) are identical when the areas are calculated 
-# directly from the shapefile
-rco_glasgow <- RCO(
-  x = dta@data[,c("house", "nonhouse")],
-  folder = "shapefiles_with_attributes/2grp_2001/ttwa",
-  shape = "Glasgow_accom_2001"    
-) 
+fls <- list.files("shapefiles_with_attributes/2grp_2001/ttwa", "\\.shp$")
+task_list <- data_frame(filename =fls) %>% 
+  mutate(tmp = str_replace(filename, "\\.shp$", "")) %>% 
+  separate(tmp, into = c("place", "attribute", "year"), sep = "_")
 
 
+# First, create a function which saves each shapefile into an element of a not-quite dataframe
+
+
+get_shapefiles <- function(filename){
+  filename_short <- str_replace(filename, "\\.shp$", "")
+  this_shp <- readOGR(
+    dsn = "shapefiles_with_attributes/2grp_2001/ttwa",
+    layer = filename_short
+  )
+  
+  return(this_shp)
+}
+
+task_list <- task_list %>% 
+  mutate(shp = map(filename, get_shapefiles)) 
+
+# extract data portion of each object
+task_list <- task_list %>% 
+  mutate(dta = map(shp, ~ slot(., "data"))) 
+
+# Now neighbourhood matrix
+
+get_nhd <- function(x){
+  
+  output <- x %>% 
+    poly2nb(., queen = FALSE) %>%
+    nb2mat(., style = "B", zero.policy = T)
+  return(output)
+}
+
+task_list <- task_list %>% 
+  mutate(nhd = map(shp, get_nhd)) 
+
+# Now to add the measures one by one 
+
+get_simple_d <- function(x){
+  counts_matrix <- x[,c(8, 9)]
+  dissim <- OasisR::DI(counts_matrix)
+  output <- dissim[2,1]
+  return(output)
+}
+# Calculate indices for each separately 
+
+get_adj_d <- function(x, y){
+  counts_matrix <- x[,c(8, 9)]
+  output <- OasisR::Morill(counts_matrix, y)  
+  return(output)
+}
+# Morill(dta@data[,c("house", "nonhouse")], nhd_matrix) 
+debug(get_adj_d)
+
+task_list %>% 
+  mutate(
+    d_simple = map_dbl(dta, get_simple_d),
+    d_adj = map2(dta, nhd, get_adj_d)
+  )
+
+# fn <- function(x){
+#   dta <- readOGR(
+#     dsn = "shapefiles_with_attributes/2grp_2001",
+#     layer = "accom_2001"                     
+#   )    
+#   
+#   nhd_matrix <- nb2mat(
+#     poly2nb(
+#       dta, queen = FALSE),
+#     style = "B",
+#     zero.policy = T
+#   ) # NOTE: Takes a while to calculate!
+#   
+#   
+#   
+#   # unevenness 
+#   
+#   dissim_oasis <- DI(dta@data[,c("house", "nonhouse")])
+#   
+#   d_adj_oasis <- Morill(dta@data[,c("house", "nonhouse")], nhd_matrix) 
+#   
+#   adj <- dissim_oasis - d_adj_oasis
+#   
+#   
+#   # dissim_scores <- dissim(
+#   #   x = dta, 
+#   #   data = dta@data[,c("house", "nonhouse")],
+#   #   nb = nhd_matrix,
+#   #   adjust = T
+#   #   )
+#   
+#   # Fails for spatially adjusted measures 
+#   # because cannot handle empty neighbours 
+#   
+#   
+#   # isolation
+#   
+#   iso_scores2 <- OasisR::xPx(dta@data[,c("house", "nonhouse")])
+#   
+#   # Numbers match 
+#   
+#   # Adjusted isolation (Eta2)
+#   
+#   eta2 <- OasisR::Eta2(dta@data[,c("house", "nonhouse")])
+#   
+#   # centralisation
+#   
+#   # start with Glasgow accommodation
+#   
+#   dta <- readOGR(
+#     dsn = "shapefiles_with_attributes/2grp_2001/ttwa",
+#     layer = "Glasgow_accom_2001"                     
+#   )   
+#   
+#   distc <- distcenter(dta, center = 573)
+#   
+#   rce_glasgow <- RCE(
+#     x = dta@data[,c("house", "nonhouse")],
+#     dc = distc,
+#     center = 573
+#   )
+#   
+#   
+#   
+# }
 # 
+#   
