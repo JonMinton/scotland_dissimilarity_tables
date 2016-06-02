66ba261488864f310fd0546dd4e197e894413240
JonMinton <nate.minton@gmail.com>
2016-06-02 13:27:54 +0100

    produce new shapefiles with attributes for dz2011

--- scripts/code_for_merging_attributes_to_shapefiles.R
+++ scripts/code_for_merging_attributes_to_shapefiles.R
@@ -52,34 +52,32 @@ join_attribute_table <- function(x, y, xcol, ycol) {
 
 
 
-dz_2001_shp <- readOGR(
-  dsn = "shapefiles/scotland_2001_datazones",
-  layer = "scotland_dz_2001"                     
+dz_2011_shp <- readOGR(
+  dsn = "shapefiles/SG_DataZoneBdry_2011",
+  layer = "SG_DataZone_Bdry_2011"                     
 )      
 
 
 # csv locations 
-dir("output_data/dz_2001/binary/")
-
-att_files <- dir("output_data/dz_2001/binary")
+att_files <- dir("output_data/dz_2011/binary")
 
 
 
 fn <- function(x){
-  infile <- read_csv(paste0("output_data/dz_2001/binary/", x))
+  infile <- read_csv(paste0("output_data/dz_2011/binary/", x))
   
   infile <- infile[!duplicated(infile),]
   
-  shp_joined <- dz_2001_shp
+  shp_joined <- dz_2011_shp
   
-  shp_joined <- join_attribute_table(dz_2001_shp, infile, "zonecode", "dz_2001")
+  shp_joined <- join_attribute_table(dz_2011_shp, infile, "DataZone", "dz_2011")
   
   
   
   
   outname <- x %>% str_replace("\\.csv$", "")
   
-  writeOGR(shp_joined, dsn = "shapefiles_with_attributes/2grp_2001", layer = outname,  driver = "ESRI Shapefile")
+  writeOGR(shp_joined, dsn = "shapefiles_with_attributes/2grp_2011", layer = outname,  driver = "ESRI Shapefile")
   
   
   return(NULL)
@@ -88,12 +86,14 @@ fn <- function(x){
 
 l_ply(att_files, fn, .progress = "text")
 
-# Code exploring GIDs and datazones from 2001 dz shapefile.
 
-# There are more GIDs than datazones. I think this is because some datazones include more than 
-# one polygon. This would make sense for island areas in particular, as the minimum size of datazones 
-# means they are likely to include more than one non-contiguous polygon. 
+# This produces MANY warning messages, examples below:
 
+# Warning 1: Value 218966759.866 of field Shape_Area of feature 1149 not successfully written. Possibly due to too larger number with respect to field width
+# Warning 1: Value 201699500.39399999 of field Shape_Area of feature 1364 not successfully written. Possibly due to too larger number with respect to field width
+#
+
+# However, the shapefiles appear to work. Shape_Area fields are visible. 
 
 
 # produce separate shapfiles for separate TTWAs ---------------------------
@@ -101,15 +101,20 @@ l_ply(att_files, fn, .progress = "text")
 ttwas <- c("Aberdeen", "Glasgow", "Edinburgh", "Dundee", 
            "Inverness and Dingwall", "Perth and Blairgowrie", "Stirling and Alloa")
 
-att_files <- dir("output_data/dz_2001/binary")
+att_files <- dir("output_data/dz_2011/binary")
 combinations <- expand.grid(attributes = att_files, ttwa = ttwas)
 
 # so, the aim is to produce smaller joins for each ttwa, and to label each year/attribute/ttwa shapefile appropriately
-ttwa <- read_csv(file = "input_data/lookups/LSOA01_TTWA01_UK_LU.csv", col_types = "ccccccc")
+ttwa <- read_csv(file = "input_data/lookups/LSOA DZ SOA to TTWA Lookup_V2.csv", col_types = "ccccc")
 
 # Need to state centroids for each ttwa
+# postcode_to_oa <- read_csv(file = "input_data/lookups/POSTCODE_TO_OA.csv")
+# norman <- read_csv("input_data/lookups/paul_norman_dz2011_table.csv")
+# postcode_to_oa %>% 
+#   select(postcode = Postcode, oa_2011 = OutputArea2011Code) %>% 
+#   inner_join(norman) %>% 
+#   filter(postcode %in% c("G1 3BU", "EH1 1BQ", "AB10 1AN", "DD1 2AJ", "PH2 8PA", "IV1 1HY", "FK8 2LJ"))
 
- 
 # ACTION: Ask Gwilym for his defintions of centres for ttwas
 
 
@@ -118,49 +123,67 @@ ttwa <- read_csv(file = "input_data/lookups/LSOA01_TTWA01_UK_LU.csv", col_types 
 # Glasgow
 #  - West End of George Square
 # G1 3BU
-# S01003358
+# dz_2001 : S01003358
+# dz_2011 : S01010265
 
 # Edinburgh 
 # - 31 Waverley Bridge
 # EH1 1BQ
-# S01002131
-
+# dz_2001 : S01002131
+# dz_2011 : S01008677
 
 # Aberdeen
 # - Shoe Lane
 # AB10 1AN
-# S01000126
+# dz_2001 : S01000126
+# dz_2011 : S01006646
 
 # Dundee 
 # - Commercial Street
 # DD1 2AJ
-# S01001101
+# dz_2001 : S01001101
+# dz_2001 : S01007705
 
 # Perth 
 # - South Street
 # PH2 8PA
-# S01005037
+# dz_2001 : S01005037
+# dz_2011 : S01011939 (option A)
+# dz_2011 : S01011940 (option B)
+
 
 # Inverness 
 # - High Street/Castle Street
 # IV1 1HY
-# S01003853
+# dz_2001 : S01003853
+# dz_2011 : S01010620
 
 # Stirling
 # Port Street
 # FK8 2LJ
-# S01006120
+# dz_2001 : S01006120
+# dz_2011 : S01013067
 
+# 2001 centroids
+# ttwa_centroids <- c(
+#   Aberdeen = "S01000125",
+#   Glasgow = "S01003358",
+#   Edinburgh = "S01002131", 
+#   Dundee = "S01001101",
+#   `Inverness and Dingwall` = "S01003853",
+#   `Perth and Blairgowrie` = "S01005037",
+#   `Stirling and Alloa` = "S01006120"
+#   )
 
-
+# 2011 centroids
 ttwa_centroids <- c(
-  Aberdeen = "S01000125",
-  Glasgow = "S01003358",
-  Edinburgh = "S01002131", 
-  Dundee = "S01001101",
-  `Inverness and Dingwall` = "S01003853",
-  `Perth and Blairgowrie` = "S01005037",
-  `Stirling and Alloa` = "S01006120"
+  Aberdeen = "S01006646",
+  Glasgow = "S01010265",
+  Edinburgh = "S01008677",
+  Dundee = "S01007705",
+  `Inverness and Dingwall` = "S01010620",
+  `Perth and Blairgowrie` = "S01011939",
+  `Stirling and Alloa` = "S01013067"
   )
 
 
@@ -168,205 +191,31 @@ fn <- function(x){
   # x now contains 
   # x$attributes 
   # x$ttwa 
-  infile <- read_csv(paste0("output_data/dz_2001/binary/", x$attributes))
+  infile <- read_csv(paste0("output_data/dz_2011/binary/", x$attributes))
   infile <- infile[!duplicated(infile),]
   # Now need to add one more column: centre
   # 1 if city centre; 0 otherwise
   infile <- infile %>% mutate(
-    centre = ifelse(dz_2001 == ttwa_centroids[x$ttwa], 1, 0)
+    centre = ifelse(dz_2011 == ttwa_centroids[x$ttwa], 1, 0)
   )
   
-  dzs_in_selection <- ttwa %>% filter(TTWA01NM == x$ttwa) %>% .$LSOA01CD %>% unique
+  dzs_in_selection <- ttwa %>% filter(TTWA11NM == x$ttwa) %>% .$LSOA11CD %>% unique
 
-  shp_joined <- join_attribute_table(dz_2001_shp, infile, "zonecode", "dz_2001")
+  shp_joined <- join_attribute_table(dz_2011_shp, infile, "DataZone", "dz_2011")
 
-  shp_joined <- shp_joined[shp_joined$zonecode %in% dzs_in_selection,]
+  shp_joined <- shp_joined[shp_joined$DataZone %in% dzs_in_selection,]
   
   outname <- paste0(x$ttwa, "_", str_replace(x$attributes, "\\.csv$", ""))
   plot(shp_joined, main = outname)
 
-  writeOGR(shp_joined, dsn = "shapefiles_with_attributes/2grp_2001/ttwa", layer = outname,  driver = "ESRI Shapefile")
+  writeOGR(shp_joined, dsn = "shapefiles_with_attributes/2grp_2011/ttwa", layer = outname,  driver = "ESRI Shapefile")
   
   
   return(NULL)
 }
 
-#debug(fn)
+
 
 a_ply(combinations, 1, fn, .progress = "text")
 
-
-# This seems to have worked programmatically, but the TTWA areas are non-contiguous and so look like they aren't correct
-
-# to check this more carefully I'm now going to plot the shapefiles, highlighting particular ttwas
-
-
-
-# 
-# > require(spdep)
-# Loading required package: spdep
-# Loading required package: sp
-# Loading required package: Matrix
-# 
-# Attaching package: ‘Matrix’
-# 
-# The following object is masked from ‘package:tidyr’:
-#   
-#   expand
-# 
-# Warning message:
-#   package ‘spdep’ was built under R version 3.1.3 
-# > require(maptools)
-# Loading required package: maptools
-# Checking rgeos availability: TRUE
-# > require(rgdal)
-# Loading required package: rgdal
-# rgdal: version: 0.9-1, (SVN revision 518)
-# Geospatial Data Abstraction Library extensions to R successfully loaded
-# Loaded GDAL runtime: GDAL 1.11.1, released 2014/09/24
-# Path to GDAL shared files: \\cfsk18.campus.gla.ac.uk/SSD_Home_Data_X/jm383x/My Documents/R/win-library/3.1/rgdal/gdal
-# GDAL does not use iconv for recoding strings.
-# Loaded PROJ.4 runtime: Rel. 4.8.0, 6 March 2012, [PJ_VERSION: 480]
-# Path to PROJ.4 shared files: \\cfsk18.campus.gla.ac.uk/SSD_Home_Data_X/jm383x/My Documents/R/win-library/3.1/rgdal/proj
-# > 
-#   > require(stringr)
-# > require(plyr)
-# > require(tidyr)
-# > require(dplyr)
-# > dz_2001_shp <- readOGR(
-#   +   dsn = "shapefiles/scotland_2001_datazones",
-#   +   layer = "scotland_dz_2001"                     
-#   + )
-# OGR data source with driver: ESRI Shapefile 
-# Source: "shapefiles/scotland_2001_datazones", layer: "scotland_dz_2001"
-# with 6610 features and 5 fields
-# Feature type: wkbPolygon with 2 dimensions
-# > tmp <- dz_2001_shp
-# > tmp <- tmp  %>% tbl_df
-# Error: data is not a data frame
-# > tmp <- dz_2001_shp@data
-# > tmp <- tmp  %>% tbl_df
-# > tmp
-# Source: local data frame [6,610 x 5]
-# 
-# gid  zonecode ons_code       label name
-# 1    1 S01006490       RH 31S01006490   NA
-# 2    2 S01006505       RH 31S01006505   NA
-# 3    3 S01006499       RH 31S01006499   NA
-# 4    4 S01006494       RH 31S01006494   NA
-# 5    5 S01006397       RH 31S01006397   NA
-# 6    6 S01006456       RH 31S01006456   NA
-# 7    7 S01006422       RH 31S01006422   NA
-# 8    8 S01006418       RH 31S01006418   NA
-# 9    9 S01006429       RH 31S01006429   NA
-# 10  10 S01006419       RH 31S01006419   NA
-# .. ...       ...      ...         ...  ...
-# > length(unique(tmp$zonecode))
-# [1] 6505
-# > View(tmp)
-# > tmp  %>% arrange(zonecode)
-# Source: local data frame [6,610 x 5]
-# 
-# gid  zonecode ons_code       label name
-# 1  6089 S01000001       QA 01S01000001   NA
-# 2  6090 S01000002       QA 01S01000002   NA
-# 3  6091 S01000003       QA 01S01000003   NA
-# 4  6092 S01000004       QA 01S01000004   NA
-# 5  6093 S01000005       QA 01S01000005   NA
-# 6  6094 S01000006       QA 01S01000006   NA
-# 7  6095 S01000007       QA 01S01000007   NA
-# 8  6096 S01000008       QA 01S01000008   NA
-# 9  6097 S01000009       QA 01S01000009   NA
-# 10 6098 S01000010       QA 01S01000010   NA
-# ..  ...       ...      ...         ...  ...
-# > tmp  %>% arrange(zonecode)  %>% mutate(x = NA, x = lag(zonecode))
-# Source: local data frame [6,610 x 6]
-# 
-# gid  zonecode ons_code       label name  x
-# 1  6089 S01000001       QA 01S01000001   NA NA
-# 2  6090 S01000002       QA 01S01000002   NA  1
-# 3  6091 S01000003       QA 01S01000003   NA  2
-# 4  6092 S01000004       QA 01S01000004   NA  3
-# 5  6093 S01000005       QA 01S01000005   NA  4
-# 6  6094 S01000006       QA 01S01000006   NA  5
-# 7  6095 S01000007       QA 01S01000007   NA  6
-# 8  6096 S01000008       QA 01S01000008   NA  7
-# 9  6097 S01000009       QA 01S01000009   NA  8
-# 10 6098 S01000010       QA 01S01000010   NA  9
-# ..  ...       ...      ...         ...  ... ..
-# > tmp  %>% arrange(zonecode)  %>% mutate(x = lag(zonecode))
-# Source: local data frame [6,610 x 6]
-# 
-# gid  zonecode ons_code       label name  x
-# 1  6089 S01000001       QA 01S01000001   NA NA
-# 2  6090 S01000002       QA 01S01000002   NA  1
-# 3  6091 S01000003       QA 01S01000003   NA  2
-# 4  6092 S01000004       QA 01S01000004   NA  3
-# 5  6093 S01000005       QA 01S01000005   NA  4
-# 6  6094 S01000006       QA 01S01000006   NA  5
-# 7  6095 S01000007       QA 01S01000007   NA  6
-# 8  6096 S01000008       QA 01S01000008   NA  7
-# 9  6097 S01000009       QA 01S01000009   NA  8
-# 10 6098 S01000010       QA 01S01000010   NA  9
-# ..  ...       ...      ...         ...  ... ..
-# > ?lag
-# > ?dplyr::lag.default
-# > lapply(tmp, class)
-# $gid
-# [1] "integer"
-# 
-# $zonecode
-# [1] "factor"
-# 
-# $ons_code
-# [1] "factor"
-# 
-# $label
-# [1] "factor"
-# 
-# $name
-# [1] "factor"
-# 
-
-# > tmp  %>% arrange(zonecode)  %>% mutate_each(funs(as.character), -gid)  %>% 
-# mutate(x = lag(zonecode), y = x == zonecode)
-# Source: local data frame [6,610 x 7]
-# 
-# gid  zonecode ons_code       label name         x     y
-# 1  6089 S01000001       QA 01S01000001   NA        NA    NA
-# 2  6090 S01000002       QA 01S01000002   NA S01000001 FALSE
-# 3  6091 S01000003       QA 01S01000003   NA S01000002 FALSE
-# 4  6092 S01000004       QA 01S01000004   NA S01000003 FALSE
-# 5  6093 S01000005       QA 01S01000005   NA S01000004 FALSE
-# 6  6094 S01000006       QA 01S01000006   NA S01000005 FALSE
-# 7  6095 S01000007       QA 01S01000007   NA S01000006 FALSE
-# 8  6096 S01000008       QA 01S01000008   NA S01000007 FALSE
-# 9  6097 S01000009       QA 01S01000009   NA S01000008 FALSE
-# 10 6098 S01000010       QA 01S01000010   NA S01000009 FALSE
-# ..  ...       ...      ...         ...  ...       ...   ...
-
-# > tmp  %>% arrange(zonecode)  %>% mutate_each(funs(as.character), -gid)  %>% mutate(x = lag(zonecode), y = x == zonecode)   %>% xtabs( ~ y, data = .)
-# y
-# FALSE  TRUE 
-# 6504   105 
-
-
-# > tmp  %>% arrange(zonecode)  %>% 
-# mutate_each(funs(as.character), -gid)  %>% 
-#   mutate(x = lag(zonecode), y = x == zonecode)  %>% 
-#   filter(y==T)
-# Source: local data frame [105 x 7]
-# 
-# gid  zonecode ons_code       label name         x    y
-# 1  6610 S01000105       QA 01S01000105   NA S01000105 TRUE
-# 2  6604 S01000481       QB 02S01000481   NA S01000481 TRUE
-# 3  6576 S01000711       QD 04S01000711   NA S01000711 TRUE
-# 4  6577 S01000711       QD 04S01000711   NA S01000711 TRUE
-# 5  6574 S01000721       QD 04S01000721   NA S01000721 TRUE
-# 6  6570 S01000755       QD 04S01000755   NA S01000755 TRUE
-# 7  6571 S01000755       QD 04S01000755   NA S01000755 TRUE
-# 8  6572 S01000755       QD 04S01000755   NA S01000755 TRUE
-# 9  6568 S01000763       QD 04S01000763   NA S01000763 TRUE
-# 10 6564 S01000796       QD 04S01000796   NA S01000796 TRUE
-# ..  ...       ...      ...         ...  ...       ...  ...
 
--- scripts/produce_two_category_versions_of_harmonised_tables.R
+++ scripts/produce_two_category_versions_of_harmonised_tables.R
@@ -243,3 +243,259 @@ write_csv(x = employed_2_t2, path = "output_data/dz_2001/binary/employed_2011.cs
 write_csv(x = ecact_2_t1, path = "output_data/dz_2001/binary/inactive_2001.csv")
 write_csv(x = ecact_2_t2, path = "output_data/dz_2001/binary/inactive_2011.csv")
 
+
+##As above but dz_2011
+
+
+
+
+# accom
+# car
+# cob
+# ecact
+# eth 
+# general health
+# llti
+# marital status
+# nssec
+# religion
+# student pensioner
+# tenure
+# employed/not employed
+# economic inactive
+
+
+
+# accom
+accom_n_t1 <- read_csv("output_data/dz_2011/accom_2001.csv")
+accom_n_t2 <- read_csv("output_data/dz_2011/accom_2011.csv")
+
+# house/nonhouse
+
+accom_2_t1 <- accom_n_t1 %>% transmute(dz_2011, total, house, nonhouse = flat + caravan + shared)
+# checked = all OK 
+
+accom_2_t2 <- accom_n_t2 %>% transmute(dz_2011, total, house, nonhouse = flat + caravan + shared)
+# checked - all OK
+
+# car 
+car_n_t1 <- read_csv("output_data/dz_2011/car_2001.csv")
+car_n_t2 <- read_csv("output_data/dz_2011/car_2011.csv")
+
+# inconsistency between t1 and t2# t1 already in correct form
+
+car_2_t1 <- car_n_t1
+car_2_t2 <- car_n_t2 %>% transmute(dz_2011, total, none, some = one + two + three + four)
+
+# both ok now and checked 
+
+
+
+# cob 
+
+cob_n_t1 <- read_csv("output_data/dz_2011/cob_2001.csv")
+cob_n_t2 <- read_csv("output_data/dz_2011/cob_2011.csv")
+
+cob_2_t1 <- cob_n_t1 %>% transmute(dz_2011, total, scotland, nonscot = ruk + elsewhere)
+cob_2_t2 <- cob_n_t2 %>% transmute(dz_2011, total, scotland, nonscot = ruk + elsewhere)
+
+# checked, both OK
+
+
+# general health 
+
+gh_n_t1 <- read_csv("output_data/dz_2011/general_health_2001.csv")
+gh_n_t2 <- read_csv("output_data/dz_2011/general_health_2011.csv")
+# already binary
+gh_2_t1 <- gh_n_t1
+gh_2_t2 <- gh_n_t2
+
+
+# llti
+
+llti_n_t1 <- read_csv("output_data/dz_2011/llti_2001.csv")
+llti_n_t2 <- read_csv("output_data/dz_2011/llti_2011.csv")
+# already binary
+llti_2_t1 <- llti_n_t1
+llti_2_t2 <- llti_n_t2
+
+
+# marstat
+
+ms_n_t1 <- read_csv("output_data/dz_2011/marital_status_2001.csv")
+ms_n_t2 <- read_csv("output_data/dz_2011/marital_status_2011.csv")
+
+# different categories 
+
+ms_2_t1 <- ms_n_t1 %>% 
+  transmute(
+    dz_2011, 
+    total, 
+    single , 
+    married 
+  )
+
+# checked t1, ok
+
+# checked t2, ok
+
+ms_2_t2 <- ms_n_t2
+
+
+
+## nssec
+
+nssec_n_t1 <- read_csv("output_data/dz_2011/nssec_2001.csv")
+nssec_n_t2 <- read_csv("output_data/dz_2011/nssec_2011.csv")
+
+# for this should exclude students and other, then recalc total, and make binary break above intermed
+
+nssec_2_t1 <- nssec_n_t1 %>% transmute(
+  dz_2011, 
+  higher = higher_man + lower_man, 
+  lower = small_self + intermed + routine
+) %>% 
+  mutate(total = higher + lower) %>% 
+  select(dz_2011, total, higher, lower) 
+
+nssec_2_t2 <- nssec_n_t2 %>% transmute(
+  dz_2011, 
+  higher = higher_man + lower_man, 
+  lower = small_self + intermed + routine
+) %>% 
+  mutate(total = higher + lower) %>% 
+  select(dz_2011, total, higher, lower) 
+
+
+# religion
+
+rel_n_t1 <- read_csv("output_data/dz_2011/religion_2001.csv")
+rel_n_t2 <- read_csv("output_data/dz_2011/religion_2011.csv")
+
+
+# divide - religious/nonreligious
+
+rel_2_t1 <- rel_n_t1 %>% 
+  transmute(dz_2011, total, 
+            religious = non_catholic_christian + catholic_christian + jewish + muslim + other, 
+            nonreligious = none)
+# checked, OK
+rel_2_t2 <- rel_n_t2 %>% 
+  transmute(dz_2011, total, 
+            religious = non_catholic_christian + catholic_christian + jewish + muslim + other, 
+            nonreligious = none)
+
+# checked, ok
+
+
+# student, pensioner or other
+spo_n_t1 <- read_csv("output_data/dz_2011/studentspensionersother_2001.csv")
+spo_n_t2 <- read_csv("output_data/dz_2011/studentspensionersother_2011.csv")
+
+po_2_t1 <- spo_n_t1 %>% transmute(dz_2011, total, pensioner, nonpensioner = student + other)
+po_2_t2 <- spo_n_t2 %>% transmute(dz_2011, total, pensioner, nonpensioner = student + other)
+# checked both, OK
+so_2_t1 <- spo_n_t1 %>% transmute(dz_2011, total, student, nonstudent = pensioner + other)
+so_2_t2 <- spo_n_t2 %>% transmute(dz_2011, total, student, nonstudent = pensioner + other)
+
+
+
+# tenure 
+
+tenure_n_t1 <- read_csv("output_data/dz_2011/tenure_2001.csv")
+tenure_n_t2 <- read_csv("output_data/dz_2011/tenure_2011.csv")
+
+# owned vs nonowned 
+
+tenure_2_t1 <- tenure_n_t1 %>% 
+  transmute(dz_2011, total, owned, nonowned = social + rented + rentfree)
+
+tenure_2_t2 <- tenure_n_t2 %>% 
+  transmute(dz_2011, total, owned, nonowned = social + rented + rentfree)
+# checked both, OK
+
+
+
+# employed/not employed
+
+employed_n_t1 <- read_csv("output_data/dz_2011/economic_activity_bigger_2001.csv")
+employed_n_t2 <- read_csv("output_data/dz_2011/economic_activity_bigger_2011.csv")
+
+employed_2_t1 <- employed_n_t1 %>% 
+  transmute(dz_2011, total, employed, nonemployed = unemployed + student + retired + homemaker + sick + inactive_other)
+
+employed_2_t2 <- employed_n_t2 %>% 
+  transmute(dz_2011, total, employed, nonemployed = unemployed + student + retired + homemaker + sick + inactive_other)
+
+
+# economic inactive 
+
+ecact_n_t1 <- read_csv("output_data/dz_2011/economic_activity_simple_2001.csv")
+ecact_n_t2 <- read_csv("output_data/dz_2011/economic_activity_simple_2011.csv")
+
+# already binary
+
+ecact_2_t1 <- ecact_n_t1
+ecact_2_t2 <- ecact_n_t2
+
+
+# Ethnicity
+
+
+eth_n_t1 <- read_csv("output_data/dz_2011/eth_2001.csv")
+eth_n_t2 <- read_csv("output_data/dz_2011/eth_2011.csv")
+
+eth_2_t1 <- eth_n_t1 %>% 
+  transmute(dz_2011, total, white, nonwhite = pakistani + chinese + other)
+
+eth_2_t2 <- eth_n_t2 %>% 
+  transmute(dz_2011, total, white, nonwhite = pakistani + chinese + other)
+
+
+# now to write these all out 
+#dir.create("output_data/dz_2011/binary/")
+write_csv(x = accom_2_t1, path = "output_data/dz_2011/binary/accom_2001.csv")
+write_csv(x = accom_2_t2, path = "output_data/dz_2011/binary/accom_2011.csv")
+
+write_csv(x = car_2_t1, path = "output_data/dz_2011/binary/car_2001.csv")
+write_csv(x = car_2_t2, path = "output_data/dz_2011/binary/car_2011.csv")
+
+write_csv(x = cob_2_t1, path = "output_data/dz_2011/binary/cob_2001.csv")
+write_csv(x = cob_2_t2, path = "output_data/dz_2011/binary/cob_2011.csv")
+
+write_csv(x = eth_2_t1, path = "output_data/dz_2011/binary/eth_2001.csv")
+write_csv(x = eth_2_t2, path = "output_data/dz_2011/binary/eth_2011.csv")
+
+
+write_csv(x = gh_2_t1, path = "output_data/dz_2011/binary/generalhealth_2001.csv")
+write_csv(x = gh_2_t2, path = "output_data/dz_2011/binary/generalhealth_2011.csv")
+
+write_csv(x = llti_2_t1, path = "output_data/dz_2011/binary/llti_2001.csv")
+write_csv(x = llti_2_t2, path = "output_data/dz_2011/binary/llti_2011.csv")
+
+write_csv(x = ms_2_t1, path = "output_data/dz_2011/binary/maritalstatus_2001.csv")
+write_csv(x = ms_2_t2, path = "output_data/dz_2011/binary/maritalstatus_2011.csv")
+
+write_csv(x = nssec_2_t1, path = "output_data/dz_2011/binary/nssec_2001.csv")
+write_csv(x = nssec_2_t2, path = "output_data/dz_2011/binary/nssec_2011.csv")
+
+write_csv(x = rel_2_t1, path = "output_data/dz_2011/binary/religion_2001.csv")
+write_csv(x = rel_2_t2, path = "output_data/dz_2011/binary/religion_2011.csv")
+
+write_csv(x = po_2_t1, path = "output_data/dz_2011/binary/pensioners_2001.csv")
+write_csv(x = po_2_t2, path = "output_data/dz_2011/binary/pensioners_2011.csv")
+
+write_csv(x = so_2_t1, path = "output_data/dz_2011/binary/students_2001.csv")
+write_csv(x = so_2_t2, path = "output_data/dz_2011/binary/students_2011.csv")
+
+
+write_csv(x = tenure_2_t1, path = "output_data/dz_2011/binary/homeowners_2001.csv")
+write_csv(x = tenure_2_t2, path = "output_data/dz_2011/binary/homeowners_2011.csv")
+
+
+write_csv(x = employed_2_t1, path = "output_data/dz_2011/binary/employed_2001.csv")
+write_csv(x = employed_2_t2, path = "output_data/dz_2011/binary/employed_2011.csv")
+
+write_csv(x = ecact_2_t1, path = "output_data/dz_2011/binary/inactive_2001.csv")
+write_csv(x = ecact_2_t2, path = "output_data/dz_2011/binary/inactive_2011.csv")
+
